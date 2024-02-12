45ee61cd8a9f63df8c131e4190e860192b39f8bb
vijaybareddy <vijaybareddy143@gmail.com>
2024-02-12 11:15:40 +0530

    hi

---
+++ src/vijay.java
@@ -0,0 +1,23 @@
+public class Vijay {
+    public String name;
+    public int rollNo;
+    public long phNo;
+    public String sec;
+
+
+}
+
+public class Naveen{
+    public String name;
+    public int rollNo;
+    public long phNo;
+
+    public String sec;
+}
+public class Reddy{
+    public String name;
+    public int rollNo;
+    public long phNo;
+    public String sec;
+
+}
